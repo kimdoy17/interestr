e0ceed235fd9a245946b39a65e071cfedcb7c6da
Michael Minhas <michael.minhas@hotmail.com>
2018-11-21 13:35:40 -0800

    add front end

---
+++ controller/controller_signup.js
@@ -0,0 +1,12 @@
+addUser = () => {
+    console.log("sign up user");
+}
+
+loginUser = () => {
+    console.log("login user");
+    window.location = 'index.html';
+}
+
+badgebook = () => {
+    console.log("connect with badgebook");
+}
